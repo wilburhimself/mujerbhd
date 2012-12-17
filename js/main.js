2e79b5193ec88cf044c3a1d5a8969bc11ed8f1fb
Wilbur Suero <wilbur.himself@gmail.com>
2012-12-17 07:55:49 -0400

    hot fixes

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 $(document).ready(function() {
     $('.accordion a').click(function(e) {
-        e.preventDefault();
-        $(".ac-content").hide("slow");
-        $(this).next(".ac-content").show("slow");
+        var n = $(this).next(".ac-content");
+        if (n.css('display') !== 'none') {
+            n.hide("slow");
+        } else {
+            n.show("slow");
+        } 
+
         return false;
     })    
 });
